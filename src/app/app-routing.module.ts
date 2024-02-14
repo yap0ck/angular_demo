6c0eb9fb4213d1eba0675fc30440f949d6084d6d
Beukens <beukensgaetan@gmail.com>
2024-02-14 15:26:56 +0100

    Update UI structure and add new components
    Removed old header and revamped the application UI. Several new components such as a shared header, navbar, and footer have been created and added to the root application module. Updated styles across multiple components to match the new layout. Addressed routing enhancements for new routes and redirection. Additional changes include relocation of the header component to shared module and minor adjustments to the HomeComponent styling.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,11 @@ import {HomeComponent} from "./home/home.component";
 import {ContactComponent} from "./contact/contact.component";
 
 const routes: Routes = [
+  {path: '', redirectTo:'/home'},
   {path:'home', component:HomeComponent},
-  {path:'contact', component:ContactComponent}
+  {path:'contact', component:ContactComponent},
+  {path: 'demo', loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)},
+  {path: 'exo', loadChildren: () => import('./exo/exo.module').then(m => m.ExoModule)}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { DemoComponent } from './demo/demo.component';
+import { ExoComponent } from './exo/exo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    DemoComponent,
+    ExoComponent
   ],
   imports: [
     BrowserModule,
