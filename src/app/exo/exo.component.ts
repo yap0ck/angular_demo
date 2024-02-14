8f90a4885f7d4199abcb0262f39b18055ae3f572
Beukens <beukensgaetan@gmail.com>
2024-02-14 15:34:05 +0100

    Create new 'demo' and 'exo' Angular components
    Introduced new 'demo' and 'exo' Angular components along with their corresponding modules and routing configurations. These new components have been integrated into the existing application structure. Furthermore, basic HTML templates have been placed in both the components, displaying a static message and a router-outlet for potential nested routing. The aim is to expand the application with new features encapsulated within these components.

---
+++ src/app/exo/exo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exo',
+  templateUrl: './exo.component.html',
+  styleUrl: './exo.component.css'
+})
+export class ExoComponent {
+
+}
---
+++ src/app/exo/exo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExoRoutingModule } from './exo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExoRoutingModule
+  ]
+})
+export class ExoModule { }
